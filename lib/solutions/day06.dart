2ef7f1296c4f960f4454197348e0ab5a30786659
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-12-06 22:11:56 +0100

    slightly better syntax

--- lib/solutions/day06.dart
+++ lib/solutions/day06.dart
@@ -6,7 +6,7 @@ class Day06 extends GenericDay {
   Day06([this.inType = 'in']) : super(6, inType);
 
   @override
-  (Iterable<int>, Iterable<int>) parseInput() {
+  ({Iterable<int> times, Iterable<int> distances}) parseInput() {
     final lines = input.getPerLine();
     RegExp exp = RegExp(r'\d+');
     Iterable<int> times =
@@ -17,7 +17,7 @@ class Day06 extends GenericDay {
         exp.allMatches(lines[1]).map((e) => int.tryParse(e[0]!)).map(
               (e) => e!,
             );
-    return (times, distances);
+    return (times: times, distances: distances);
   }
 
   int calculation(int input, int duration) {
@@ -27,14 +27,14 @@ class Day06 extends GenericDay {
   @override
   int solvePartA() {
     final input = parseInput();
-    Iterable<int> times = input.$1;
-    List<int> distances = input.$2.toList();
+    Iterable<int> times = input.times;
+    List<int> distances = input.distances.toList();
     int result = 1;
-    for (var time in times.indexed) {
+    for (var (index, time) in times.indexed) {
       int myWins = 1
-          .rangeTo(time.$2)
-          .map((e) => calculation(e, time.$2))
-          .count((e) => e > distances[time.$1]);
+          .rangeTo(time)
+          .map((e) => calculation(e, time))
+          .count((e) => e > distances[index]);
       result *= myWins;
     }
     return result;
@@ -43,8 +43,8 @@ class Day06 extends GenericDay {
   @override
   int solvePartB() {
     final input = parseInput();
-    int time = input.$1.join().toInt();
-    int distance = input.$2.join().toInt();
+    int time = input.times.join().toInt();
+    int distance = input.distances.join().toInt();
     return 1
         .rangeTo(time)
         .map((e) => calculation(e, time))

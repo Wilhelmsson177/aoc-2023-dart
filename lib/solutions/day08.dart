d82486f6b3862be51108e352197691e4aaa6d158
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-12-08 07:49:21 +0100

    part A

--- lib/solutions/day08.dart
+++ lib/solutions/day08.dart
@@ -1,20 +1,47 @@
 import 'package:aoc/index.dart';
 
+typedef Follower = ({String left, String right});
+
 class Day08 extends GenericDay {
   final String inType;
-  Day08([this.inType='in']) : super(8, inType);
+  Day08([this.inType = 'in']) : super(8, inType);
 
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+  (String, Map<String, Follower>) parseInput() {
+    String directions = input.getPerLine().first;
+    Map<String, Follower> follower = <String, Follower>{
+      for (var v in input.getPerLine().skip(2))
+        v.substring(0, 3): (
+          left: v.substring(7, 10),
+          right: v.substring(12, 15)
+        )
+    };
+    return (directions, follower);
   }
 
   @override
   int solvePartA() {
-    final input = parseInput();
-    return 0;
+    (String, Map<String, Follower>) input = parseInput();
+    String directions = input.$1;
+    Map<String, Follower> map = input.$2;
+    int counter = 0;
+    bool foundDestination = false;
+    String next = "AAA";
+    while (!foundDestination) {
+      String nextTurn = directions.substring(
+          counter % directions.length, counter % directions.length + 1);
+
+      next = switch (nextTurn) {
+        "R" => map[next]!.right,
+        "L" => map[next]!.left,
+        _ => throw Error()
+      };
+      counter += 1;
+      if (next == "ZZZ") {
+        foundDestination = true;
+      }
+    }
+    return counter;
   }
 
   @override
@@ -23,4 +50,3 @@ class Day08 extends GenericDay {
     return 0;
   }
 }
-
--- test/day08_test.dart
+++ test/day08_test.dart
@@ -3,8 +3,28 @@ import 'package:aoc/solutions/index.dart';
 
 void main() {
   test('Day08', () async {
-    String input = "0";
-    int expectation = 0;
+    String input = """RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)""";
+    int expectation = 2;
+    var day = Day08(input);
+    expect(day.solvePartA(), expectation);
+    expect(day.solvePartB(), 0);
+  });
+
+  test('Day08 - simple', () async {
+    String input = """LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)""";
+    int expectation = 6;
     var day = Day08(input);
     expect(day.solvePartA(), expectation);
     expect(day.solvePartB(), 0);

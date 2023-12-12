ac676c7e24606364d4eac86c4b382d954ec5edca
Willi Meierhof <willi@meierhof.dev>
2023-12-12 06:13:23 +0100

    add test

--- test/day12_test.dart
+++ test/day12_test.dart
@@ -2,11 +2,27 @@ import 'package:test/test.dart';
 import 'package:aoc/solutions/index.dart';
 
 void main() {
-  test('Day12', () async {
-    String input = "0";
-    int expectation = 0;
-    var day = Day12(input);
+  test('Day12 - input test', () async {
+    String unknown = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""";
+    int expectation = 21;
+    var day = Day12(unknown);
     expect(day.solvePartA(), expectation);
-    expect(day.solvePartB(), 0);
+  });
+
+  test('Day12 - known test', () async {
+    String known = """#.#.### 1,1,3
+.#...#....###. 1,1,3
+.#.###.#.###### 1,3,1,6
+####.#...#... 4,1,1
+#....######..#####. 1,6,5
+.###.##....# 3,2,1""";
+    int expectation = 21;
+    var day = Day12(known);
+    expect(day.solvePartA(), expectation);
   });
 }

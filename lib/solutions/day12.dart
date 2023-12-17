91022ef15581265bb8a457e2e363cf2f1f698144
Willi Meierhof <willi@meierhof.dev>
2023-12-17 07:44:53 +0100

    wip

--- lib/solutions/day12.dart
+++ lib/solutions/day12.dart
@@ -1,20 +1,44 @@
 import 'package:aoc/index.dart';
+import 'package:dartx/dartx.dart';
+
+class Springs {
+  late Iterable springs;
+  late Iterable configuration;
+
+  Springs(String input) {
+    var splits = input.split(" ");
+    springs = splits.first.split("");
+    configuration = splits.last.split(",").map((e) => e.toInt());
+  }
+
+  int validPermutations() {
+    var springPermutations = [(group: 0, amount: 0, permutations: 1)];
+    final springPermutationCounts = {(group: 0, amount: 0): 1};
+    int springsChecked = 0;
+    for (final spring in springs) {
+      if (spring != '?') {}
+    }
+    return 0;
+  }
+}
 
 class Day12 extends GenericDay {
   final String inType;
-  Day12([this.inType='in']) : super(12, inType);
+  Day12([this.inType = 'in']) : super(12, inType);
 
   @override
-  List<int> parseInput() {
+  Iterable<Springs> parseInput() {
     final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+    return lines.map((e) => Springs(e));
   }
 
   @override
   int solvePartA() {
     final input = parseInput();
-    return 0;
+    return input.fold(
+        0,
+        (previousValue, element) =>
+            previousValue + element.validPermutations());
   }
 
   @override
@@ -23,4 +47,3 @@ class Day12 extends GenericDay {
     return 0;
   }
 }
-

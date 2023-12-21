dd7c319b30a4156037d4cb8b2c3f83f1e1220a40
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-12-21 11:05:04 +0100

    wip: erstmal aufhören

--- lib/solutions/day19.dart
+++ lib/solutions/day19.dart
@@ -1,25 +1,43 @@
 import 'package:aoc/index.dart';
+import 'package:aoc/logger.dart';
+import 'package:dart_eval/dart_eval.dart';
 import 'package:dartx/dartx.dart';
 
 RegExp machinePartRegExp = RegExp(r"([xmas])=(\d+)");
 RegExp workflowConditionRegExp = RegExp(r"([xmas])([><])(\d+):(.+)");
 RegExp workflowRegExp = RegExp(r"([a-zA-Z]+)\{(.+)\}");
 
-enum Comperator { greater, less }
+enum Comperator {
+  greater,
+  less;
+
+  String get value {
+    return switch (this) {
+      Comperator.greater => ">",
+      Comperator.less => "<",
+    };
+  }
+}
+
+enum CheckResult { accepted, rejected, jumpNext, stay }
 
 class WorkflowCondition {
   late String? identifier;
   late Comperator? comperator;
-  late String? next;
-  late bool reject = false;
-  late bool accept = false;
+  late int? value;
+  late final bool needsCompare;
+  late CheckResult result;
+  String? next;
 
   WorkflowCondition(String input) {
     if (input == "A") {
-      accept = true;
+      result = CheckResult.accepted;
+      needsCompare = false;
     } else if (input == "R") {
-      reject = true;
+      result = CheckResult.rejected;
+      needsCompare = false;
     } else if (workflowConditionRegExp.hasMatch(input)) {
+      needsCompare = true;
       var match = workflowConditionRegExp.firstMatch(input)!;
       identifier = match.group(1);
       comperator = switch (match.group(2)) {
@@ -27,18 +45,38 @@ class WorkflowCondition {
         "<" => Comperator.less,
         _ => throw Error()
       };
-      switch (match.group(3)) {
+      value = int.parse(match.group(3)!);
+      switch (match.group(4)) {
         case "R":
-          reject = true;
+          result = CheckResult.rejected;
           break;
         case "A":
-          accept = true;
+          result = CheckResult.accepted;
           break;
         default:
-          next = match.group(3);
+          next = match.group(4);
+          result = CheckResult.jumpNext;
       }
     } else {
       next = input;
+      needsCompare = false;
+      result = CheckResult.jumpNext;
+    }
+  }
+
+  void checkPart(MachinePart part) {
+    if (needsCompare) {
+      String expression = switch (identifier) {
+        "x" => "${part.x} ${comperator!.value} $value",
+        "m" => "${part.m} ${comperator!.value} $value",
+        "a" => "${part.a} ${comperator!.value} $value",
+        "s" => "${part.s} ${comperator!.value} $value",
+        _ => throw Error()
+      };
+      bool intermediateResult = eval(expression);
+      if (!intermediateResult) {
+        result = CheckResult.stay;
+      }
     }
   }
 }
@@ -52,10 +90,21 @@ class Workflow {
     workflows
         .addAll(match.group(2)!.split(",").map((e) => WorkflowCondition(e)));
   }
+  ({CheckResult result, String? next}) checkPart(MachinePart part) {
+    for (WorkflowCondition w in workflows) {
+      w.checkPart(part);
+      if (w.result == CheckResult.stay) {
+        continue;
+      }
+      return (result: w.result, next: w.next);
+    }
+    throw Error();
+  }
 }
 
 class WorkflowCoordinator {
   Map<String, Workflow> workflows = {};
+
   WorkflowCoordinator(String input) {
     workflows.addEntries(input
         .split("\n")
@@ -64,6 +113,22 @@ class WorkflowCoordinator {
   }
 
   bool isAccepted(MachinePart part) {
+    String next = "in";
+    do {
+      var result = workflows[next]!.checkPart(part);
+      switch (result.result) {
+        case CheckResult.accepted:
+          return true;
+        case CheckResult.rejected:
+          return false;
+        case CheckResult.jumpNext:
+          next = result.next!;
+        case CheckResult.stay:
+          continue;
+        default:
+          throw Error();
+      }
+    } while (next != "");
     return false;
   }
 }
@@ -96,6 +161,10 @@ class MachinePart {
       }
     });
   }
+  @override
+  String toString() {
+    return "{x=$x,m=$m,a=$a,s=$s}";
+  }
 }
 
 class Day19 extends GenericDay {
@@ -117,10 +186,12 @@ class Day19 extends GenericDay {
     var input = parseInput();
     MachineParts parts = input.parts;
     WorkflowCoordinator wC = input.workflowCoordinator;
-
-    return parts
-        .filter((element) => wC.isAccepted(element))
-        .fold(0, (previousValue, e) => previousValue + e.x + e.m + e.a + e.s);
+    var results = parts.map((p0) => (p0, wC.isAccepted(p0)));
+    talker.verbose(results.toList());
+    return results.filter((element) => element.$2).fold(
+        0,
+        (previousValue, e) =>
+            previousValue + e.$1.x + e.$1.m + e.$1.a + e.$1.s);
   }
 
   @override
